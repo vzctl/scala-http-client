129219e306eb4652690df9db98d7f3343580d439
Simon Kelly <simongdkelly@gmail.com>
2012-06-18 13:53:57 +0200

    De-nest Response class

--- src/main/scala/com/github/tototoshi/http/Client.scala
+++ src/main/scala/com/github/tototoshi/http/Client.scala
@@ -16,7 +16,6 @@ import net.liftweb.json.JsonParser._
 import net.liftweb.json.JValue
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager
 
-
 trait Using {
 
   type Closable = { def close(): Unit }
@@ -62,38 +61,36 @@ class Client extends Using {
     request.setEntity(new StringEntity(body, encoding))
     new Response(httpClient.execute(request))
   }
+}
 
+class Response(httpResponse: HttpResponse) extends Using {
 
-  class Response(httpResponse: HttpResponse) {
+  def statusCode(): Int = {
+    EntityUtils.consume(httpResponse.getEntity)
+    httpResponse.getStatusLine.getStatusCode
+  }
 
-    def statusCode(): Int = {
-      EntityUtils.consume(httpResponse.getEntity)
-      httpResponse.getStatusLine.getStatusCode
-    }
+  def asString(): String = asString("UTF-8")
 
-    def asString(): String = asString("UTF-8")
+  def asString(charset: String): String = {
+    val res = EntityUtils.toString(httpResponse.getEntity, charset)
+    EntityUtils.consume(httpResponse.getEntity)
+    res
+  }
 
-    def asString(charset: String): String = {
-      val res = EntityUtils.toString(httpResponse.getEntity, charset)
-      EntityUtils.consume(httpResponse.getEntity)
-      res
-    }
+  def asJson(): JValue = parse(asString)
 
-    def asJson(): JValue = parse(asString)
+  def save(filename: String): Unit = save(new File(filename))
 
-    def save(filename: String): Unit = save(new File(filename))
-
-    def save(file: File): Unit = {
-      using(new java.io.BufferedInputStream(httpResponse.getEntity.getContent)) { in =>
-        using(new java.io.PrintStream(file)) { out =>
-          val buffer = new Array[Byte](8192)
-          Stream.continually(in.read(buffer)).takeWhile(_ >= 0) foreach {
-            out.write(buffer, 0, _)
-          }
+  def save(file: File): Unit = {
+    using(new java.io.BufferedInputStream(httpResponse.getEntity.getContent)) { in =>
+      using(new java.io.PrintStream(file)) { out =>
+        val buffer = new Array[Byte](8192)
+        Stream.continually(in.read(buffer)).takeWhile(_ >= 0) foreach {
+          out.write(buffer, 0, _)
         }
       }
     }
-
   }
 
 }
